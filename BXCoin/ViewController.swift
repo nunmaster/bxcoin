c86e8558276ea828b7b279ddc3ddce54d7486ba7
Nonthanun Sudlapa <nonthanun.sud@ascendcorp.com>
2018-01-12 17:14:31 +0700

    initial commit

---
+++ BXCoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BXCoin
+//
+//  Created by Nonthanun Sudlapa on 1/12/2561 BE.
+//  Copyright © 2561 nun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-BXCoin/Pods-BXCoin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BXCoin : NSObject
+@end
+@implementation PodsDummy_Pods_BXCoin
+@end
